da98ae7992043f209bfb8ab76eb4f71901ec5f02
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-15 18:45:07 -0300

    feat(createTable.js) adicionado tabela de controle

--- src/controllers/table/createTable.js
+++ src/controllers/table/createTable.js
@@ -14,14 +14,28 @@ const createTable = async (req, res) => {
             return res.status(400).json({ message: `A tabela com nome: "${tableName}" já existe, por favor insira um nome diferente` })
         }
 
-        const createTable = await knex.schema.withSchema(usernameSchema).createTable(`${tableName}`, (table) => {
-            table.increments('product_id')
-            table.string('product_name')
-            table.integer('amount_stock')
-            table.integer('price')
-            table.string('description', 500)
-            table.string('link')
-        })
+        const findMasterTable = await knex.schema.hasTable('master_table').withSchema(usernameSchema)
+
+        if (!findMasterTable) {
+            const createMasterTable = await knex.schema.withSchema(usernameSchema)
+                .createTable('master_table', (table) => {
+                    table.increments('table_id')
+                    table.integer('user_id')
+                    table.string('table_name')
+                })
+        }
+
+        await knex('master_table').withSchema(usernameSchema).insert({ user_id: req.user.id, table_name: tableName })
+
+        const createTable = await knex.schema.withSchema(usernameSchema)
+            .createTable(`${tableName}`, (table) => {
+                table.increments('product_id')
+                table.string('product_name')
+                table.integer('amount_stock')
+                table.integer('price')
+                table.string('description', 500)
+                table.string('link')
+            })
 
         return res.status(204).send()
     } catch (error) {
