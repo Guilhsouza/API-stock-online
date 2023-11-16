2161c717349290af559118bd65bfc5d6cd969cd9
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-16 18:21:45 -0300

    style(schemas) adicionado ponto final nas messages

--- src/schemas/addProductsSchema.js
+++ src/schemas/addProductsSchema.js
@@ -5,9 +5,9 @@ const addProductsSchema = joi.object({
         .required()
         .max(255)
         .messages({
-            'any.required': 'O nome do produto é um campo obrigatório',
-            'string.empty': 'O nome do produto é um campo obrigatório',
-            'string.max': 'O nome do produto não pode ter mais que 255 caracteres'
+            'any.required': 'O nome do produto é um campo obrigatório.',
+            'string.empty': 'O nome do produto é um campo obrigatório.',
+            'string.max': 'O nome do produto não pode ter mais que 255 caracteres.'
         }),
 
     amount_stock: joi.number()
@@ -15,25 +15,25 @@ const addProductsSchema = joi.object({
         .integer()
         .positive()
         .messages({
-            'any.required': 'A quantidade no estoque é um campo obrigatório, podendo ser 0',
-            'number.base': 'A quantidade no estoque precisar ser um número válido',
-            'number.integer': 'O estoque precisa ser um número inteiro',
-            'number.positive': 'O estoque precisa ser um número positivo'
+            'any.required': 'A quantidade no estoque é um campo obrigatório, podendo ser 0.',
+            'number.base': 'A quantidade no estoque precisar ser um número válido.',
+            'number.integer': 'O estoque precisa ser um número inteiro.',
+            'number.positive': 'O estoque precisa ser um número positivo.'
         }),
 
     price: joi.number()
         .positive()
         .precision(2)
         .messages({
-            'number.base': 'O valor precisar ser um número válido',
-            'number.integer': 'O valor precisa ser um número com somente duas casas após a virgula',
-            'number.positive': 'O valor precisa ser um número positivo'
+            'number.base': 'O valor precisar ser um número válido.',
+            'number.integer': 'O valor precisa ser um número com somente duas casas após a virgula.',
+            'number.positive': 'O valor precisa ser um número positivo.'
         }),
 
     description: joi.string()
         .max(255)
         .messages({
-            'string.max': 'A descrição não pode ter mais que 500 caracteres'
+            'string.max': 'A descrição não pode ter mais que 500 caracteres.'
         }),
 
     link: joi.string()
