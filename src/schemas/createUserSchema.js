9c549c395cb792604b2c4a4bc8f386b6eff78fed
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-21 19:43:36 -0300

    style(createUserSchema.js) alterando formatação do arquivo para o mesmo padrão dos outros

--- src/schemas/createUserSchema.js
+++ src/schemas/createUserSchema.js
@@ -1,17 +1,19 @@
 const joi = require('joi')
 
 const registerValidate = joi.object({
-    first_name: joi.string().alphanum().required().messages({
-        'string.alphanum': 'O nome não pode ter caracteres especiais.',
-        'any.required': 'O nome é um campo obrigatório.',
-        'string.empty': 'O nome é um campo obrigatório.'
-    }),
+    first_name: joi.string()
+        .required()
+        .messages({
+            'any.required': 'O nome é um campo obrigatório.',
+            'string.empty': 'O nome é um campo obrigatório.'
+        }),
 
-    last_name: joi.string().alphanum().required().messages({
-        'string.alphanum': 'O sobrenome não pode ter caracteres especiais.',
-        'any.required': 'O sobrenome é um campo obrigatório.',
-        'string.empty': 'O sobrenome é um campo obrigatório.'
-    }),
+    last_name: joi.string()
+        .required()
+        .messages({
+            'any.required': 'O sobrenome é um campo obrigatório.',
+            'string.empty': 'O sobrenome é um campo obrigatório.'
+        }),
 
     cellphone_number: joi.string()
         .pattern(/^[0-9]+$/)
@@ -24,17 +26,23 @@ const registerValidate = joi.object({
             'string.length': 'O número de telefone precisa ter 11 dígitos.',
         }),
 
-    email: joi.string().email().required().messages({
-        'any.required': 'O email é um campo obrigatório.',
-        'string.empty': 'O email é um campo obrigatório.',
-        'string.email': 'O campo email precisa ter um formato valido, ex: nome@exemplo.com.'
-    }),
+    email: joi.string()
+        .email()
+        .required()
+        .messages({
+            'any.required': 'O email é um campo obrigatório.',
+            'string.empty': 'O email é um campo obrigatório.',
+            'string.email': 'O campo email precisa ter um formato valido, ex: nome@exemplo.com.'
+        }),
 
-    password: joi.string().min(6).required().messages({
-        'any.required': 'A senha é um campo obrigatório.',
-        'string.empty': 'A senha é um campo obrigatório.',
-        'string.min': 'a senha deve conter 6 ou mais caracteres.'
-    })
+    password: joi.string()
+        .min(6)
+        .required()
+        .messages({
+            'any.required': 'A senha é um campo obrigatório.',
+            'string.empty': 'A senha é um campo obrigatório.',
+            'string.min': 'a senha deve conter 6 ou mais caracteres.'
+        })
 })
 
 module.exports = registerValidate
