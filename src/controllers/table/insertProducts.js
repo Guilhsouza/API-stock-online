5202e0b75fd06cf84f75062a3beb740cb33a1581
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-22 20:19:39 -0300

    style(insertProducts.js) excluindo espaço

--- src/controllers/table/insertProducts.js
+++ src/controllers/table/insertProducts.js
@@ -21,7 +21,6 @@ const insertProducts = async (req, res) => {
 
         return res.status(201).json({ message: 'Produto criado com sucesso' })
     } catch (error) {
-
         return res.status(500).json({ message: 'Erro interno no servidor.' })
     }
 }
