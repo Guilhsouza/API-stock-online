65effff55549412e85996c1750c5216720b4f896
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-23 16:35:10 -0300

    feat(listingProducts.js) adicionado função que busca produto pelo nome

--- src/controllers/table/listingProducts.js
+++ src/controllers/table/listingProducts.js
@@ -2,13 +2,24 @@ const knex = require('../../connection/dbConnection')
 
 const listingProducts = async (req, res) => {
     const { tableName } = req.params
+    const { product_name } = req.query
     const usernameSchema = `${req.user.first_name}${req.user.id}`
 
     try {
         const tableExists = await knex.schema.hasTable(tableName).withSchema(usernameSchema)
 
         if (!tableExists) {
-            return res.status(404).json({ message: 'A tabela não foi encontrada!' })
+            return res.status(404).json({ message: 'A tabela ou o produto não foi encontrado!' })
+        }
+
+        if (product_name) {
+            const findProduct = await knex(tableName).withSchema(usernameSchema).where({ product_name })
+
+            if (!findProduct[0]) {
+                return res.status(404).json({ message: 'A tabela ou o produto não foi encontrado!' })
+            }
+
+            return res.status(200).json(findProduct)
         }
 
         const listingTable = await knex(tableName).withSchema(usernameSchema)
