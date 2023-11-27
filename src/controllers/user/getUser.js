03919d2d2043ec2334a1db2d1a17bf26c0fe937a
Guilherme Souza <gui.edu0380@gmail.com>
2023-11-27 19:31:40 -0300

    fix(getUser.js) corrigindo mensagem de erro status 404

--- src/controllers/user/getUser.js
+++ src/controllers/user/getUser.js
@@ -7,14 +7,14 @@ const getUserByID = async (req, res) => {
         const findUser = await knex('users').where({ id }).first()
 
         if (!findUser) {
-            return res.status(404).json({ message: `Usuário ${first_name} não foi encontrado(a)!` })
+            return res.status(404).json({ message: `Usuário não foi encontrado(a)!` })
         }
 
         const { password: _, ...userNotPass } = findUser
 
         return res.status(200).json(userNotPass)
     } catch (error) {
-        return req.status(500).json({ message: 'Erro interno no servidor' })
+        return res.status(500).json({ message: 'Erro interno no servidor' })
     }
 }
 
