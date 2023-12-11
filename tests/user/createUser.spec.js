af7b5ff65551efc2f3b93b23cd7b58fe601e07d0
Guilherme Souza <gui.edu0380@gmail.com>
2023-12-10 21:13:27 -0300

    test(createUser.spec.js) alterando usuário retornado para modelo

--- tests/user/createUser.spec.js
+++ tests/user/createUser.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const app = require('../../src/app')
 const knex = require('../../src/connection/dbConnection')
 const user = require('../models/userModel')
+const userNotPass = require('../models/userNotPassModel')
 
 describe('Register Users Tests', () => {
     beforeEach(async () => {
@@ -13,13 +14,9 @@ describe('Register Users Tests', () => {
 
         expect(response.statusCode).toBe(201)
         expect(response.body).toEqual(
-            expect.objectContaining({
-                id: expect.any(Number),
-                first_name: expect.any(String),
-                last_name: expect.any(String),
-                cellphone_number: expect.any(String),
-                email: expect.any(String),
-            }))
+            expect.objectContaining(
+                userNotPass
+            ))
     })
 
     it('registering a user with not data', async () => {
