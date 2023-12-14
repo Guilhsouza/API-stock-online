ef7982986db4ee11defabd119f128640aafb55fd
Guilherme Souza <gui.edu0380@gmail.com>
2023-12-14 11:46:24 -0300

    test(createUser.spec.js) adicionando mais testes na exclusão de usuário

--- tests/user/deleteUser.spec.js
+++ tests/user/deleteUser.spec.js
@@ -12,13 +12,23 @@ describe('Delete user tests', () => {
     })
 
     beforeEach(async () => {
-        return login = await request(app).post('/user/login').send(userLogin)
+        const login = await request(app).post('/user/login').send(userLogin)
+
+        return data = {
+            userId: login.body.user.id,
+            token: login.body.token
+        }
     })
 
     it('Delete user without being logged', async () => {
-        const response = await request(app).delete(`/user/${1}`).send({
-            password: '123456'
-        })
+        const token = data.token
+
+        const response = await request(app)
+            .delete(`/user/${1}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                password: '123456'
+            })
 
         expect(response.statusCode).toBe(401)
         expect(response.body).toEqual({
@@ -27,11 +37,75 @@ describe('Delete user tests', () => {
     })
 
     it('Delete user but not insert password', async () => {
-        const userId = login.body.user.id
+        const userId = data.userId
+        const token = data.token
 
-        const response = await request(app).delete(`/user/${userId}`).send({})
+        const response = await request(app)
+            .delete(`/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({})
 
         expect(response.statusCode).toBe(400)
+        expect(response.body).toEqual({
+            message: expect.any(String)
+        })
+    })
+
+    it('Delete user if wrong password', async () => {
+        const userId = data.userId
+        const token = data.token
+
+        const response = await request(app)
+            .delete(`/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                password: '12345'
+            })
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toEqual({
+            message: expect.any(String)
+        })
+    })
+
+    it('Delete user but user not exists', async () => {
+        const userId = data.userId
+        const token = data.token
+
+        await request(app)
+            .delete(`/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                password: '123456'
+            })
+
+        const response = await request(app)
+            .delete(`/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                password: '123456'
+            })
+
+        await request(app).post('/user').send(user)
+
+        expect(response.statusCode).toBe(404)
+        expect(response.body).toEqual({
+            message: expect.any(String)
+        })
+    })
+
+    it('Delete user sucessfull', async () => {
+        const userId = data.userId
+        const token = data.token
+
+        const response = await request(app)
+            .delete(`/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                password: '123456'
+            })
+
+        expect(response.statusCode).toBe(200)
         expect(response.body).toEqual({
             message: expect.any(String)
         })
