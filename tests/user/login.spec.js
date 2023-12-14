41176ebcaacdee71abafca804b69fa4237dcf100
Guilherme Souza <gui.edu0380@gmail.com>
2023-12-14 11:07:51 -0300

    test(createUser.spec.js) alterando identação dos testes de login

--- tests/user/login.spec.js
+++ tests/user/login.spec.js
@@ -7,9 +7,12 @@ const userLogin = require('../models/userLogin')
 
 describe('Login User tests', () => {
     beforeAll(async () => {
-        await knex('users').del('*')
+        await knex('users')
+            .del('*')
 
-        await request(app).post('/user').send(user)
+        await request(app)
+            .post('/user')
+            .send(user)
     })
 
     beforeEach(async () => {
@@ -17,7 +20,9 @@ describe('Login User tests', () => {
     })
 
     it('Sucessful login', async () => {
-        const response = await request(app).post('/user/login').send(userLogin)
+        const response = await request(app)
+            .post('/user/login')
+            .send(userLogin)
 
         expect(response.statusCode).toBe(200)
         expect(response.body).toEqual(
@@ -29,7 +34,9 @@ describe('Login User tests', () => {
     })
 
     it('Empty body', async () => {
-        const response = await request(app).post('/user/login').send({})
+        const response = await request(app)
+            .post('/user/login')
+            .send({})
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toEqual(
@@ -42,7 +49,9 @@ describe('Login User tests', () => {
     it('User not exists', async () => {
         userLogin.email = 'guilherme@gmail.com'
 
-        const response = await request(app).post('/user/login').send(userLogin)
+        const response = await request(app)
+            .post('/user/login')
+            .send(userLogin)
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toEqual({
@@ -53,7 +62,9 @@ describe('Login User tests', () => {
     it('invalid email', async () => {
         userLogin.email = 'guilherme@gmail'
 
-        const response = await request(app).post('/user/login').send(userLogin)
+        const response = await request(app)
+            .post('/user/login')
+            .send(userLogin)
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toEqual({
@@ -64,7 +75,9 @@ describe('Login User tests', () => {
     it('invalid password', async () => {
         userLogin.password = '654321'
 
-        const response = await request(app).post('/user/login').send(userLogin)
+        const response = await request(app)
+            .post('/user/login')
+            .send(userLogin)
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toEqual({
@@ -75,7 +88,9 @@ describe('Login User tests', () => {
     it('password not a string', async () => {
         userLogin.password = 123456
 
-        const response = await request(app).post('/user/login').send(userLogin)
+        const response = await request(app)
+            .post('/user/login')
+            .send(userLogin)
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toEqual({
